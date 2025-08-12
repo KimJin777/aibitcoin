0d1e116a75a7b53a0b3c74c1a00ce469bc2583ac
DevMachine kjin <kjink007@naver.com>
2025-08-12 16:53:46 +0900

    손절수정(수수료이상으로 남을때 팔기)

--- main.py
+++ main.py
@@ -150,25 +150,28 @@ def main_trading_cycle_with_vision(upbit, logger):
         
         current_btc_value = total_profit_loss['current_price']*total_profit_loss['btc_balance']
         my_btc_value = total_profit_loss['btc_avg_price']*total_profit_loss['btc_balance']
-        total_profit_loss_value = my_btc_value - current_btc_value
-        # print('분봉 평균', minute_df['High'][0:10].mean(), current_price)
+        total_profit_loss_value = current_btc_value - my_btc_value
+        sell_amount = total_profit_loss['btc_balance'] * 0.95  # 95% 매도
+        print('분봉 평균', minute_df['High'][0:10].mean(), current_price)
         # print('분봉 ', (minute_df['High'][0:10]))
-        # print('이익', total_profit_loss)
+        
         
         # print('비트코인 보유량', total_profit_loss['btc_balance'])
         # btc_current_value = total_profit_loss['current_price']
-        # print('현재가격', btc_current_value)
+        print('현재가격', current_btc_value)
         # my_btc_value = total_profit_loss['btc_balance']*total_profit_loss['current_price']
         # print('나의 비트코인 가치', my_btc_value)
         # total_profit_loss_value = btc_current_value - my_btc_value
         print('이익', total_profit_loss_value)
-        if average(minute_df[0:10]) > current_price and total_profit_loss_value > 50:
-            #평균가격이 현재가보다 높다는 것은 가격이 내리고 있다는 증거 
+        if (minute_df['High'][0:10].mean() > current_price) and (total_profit_loss_value > (sell_amount*0.0005)):
+            
+            #평균가격이 현재가보다 크다는 것은 가격이 내리고 있다는 증거 
             decisionSelf=True
         if decisionSelf:
             print("💼 손절 수동 매매 결정을 실행합니다...")
             decision = {'decision': 'sell'}
             execution_result = execute_trading_decision(upbit, decision, investment_status, market_data)
+
         else:
             print("💼 손절 수동 매매 실행 안합니다...")
 
